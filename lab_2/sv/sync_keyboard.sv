79ab9127f8127442c36d794264c282f12f5c8291
Khanh Nguyen <nguyenhoangkhanh271205@gmail.com>
2025-09-11 10:52:50 +0200

    Done sync_keyboard (synchronizer)

--- lab_2/sv/sync_keyboard.sv
+++ lab_2/sv/sync_keyboard.sv
@@ -1,6 +1,7 @@
 // TITLE: sync_keyboard.sv
 // PROJECT: Keyboard VLSI lab
 // DESCRIPTION: Synchronize keyboard clock and data to system clock
+// Based on Appendix 3.4
 
 `timescale 1ns/1ps
 
@@ -11,5 +12,25 @@ module sync_keyboard (
     output logic kb_clk_sync,
     output logic kb_data_sync
     );
+
+    // Two-stage synchronizer for kb_clk
+    logic kb_clk_ff1, kb_clk_ff2;
+
+    // Two-stage synchronizer for kb_data
+    logic kb_data_ff1, kb_data_ff2;
+
+    // Synchronize kb_clk
+    always_ff @(posedge clk) begin
+        kb_clk_ff1 <= kb_clk;
+        kb_clk_ff2 <= kb_clk_ff1;
+    end
+    assign kb_clk_sync = kb_clk_ff2;
+
+    // Synchronize kb_data
+    always_ff @(posedge clk) begin
+        kb_data_ff1 <= kb_data;
+        kb_data_ff2 <= kb_data_ff1;
+    end
+    assign kb_data_sync = kb_data_ff2;
 
 endmodule
