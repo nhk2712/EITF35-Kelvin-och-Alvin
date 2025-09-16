1594523e298643baad78aa7b7c43835bbcf37ca6
Alvin Karlsson <alvin.karlsson1234@gmail.com>
2025-09-16 15:43:13 +0200

    convert_scancode change valid_scan_code to be always comb

--- lab_2/sv/convert_scancode.sv
+++ lab_2/sv/convert_scancode.sv
@@ -18,15 +18,13 @@ module convert_scancode (
 
     logic [4:0] counter, counter_next; // modulo-11 counter
     logic [9:0] scan_code_temp, scan_code_temp_next; // stop, parity, 8 bits
-    logic valid_scan_code_temp, valid_scan_code_temp_next;
+    logic valid_scan_code_temp;
 
     always_ff @(posedge clk or posedge rst) begin
         if (rst) begin // rst
             scan_code_temp <= '0;
             counter <= '0;
-            valid_scan_code_temp <= '0;
         end else begin // pos clk edge
-            valid_scan_code_temp <= valid_scan_code_temp_next;
             scan_code_temp <= scan_code_temp_next;
             counter <= counter_next;
         end
@@ -36,16 +34,16 @@ module convert_scancode (
         // Default
         counter_next = counter;
         scan_code_temp_next = scan_code_temp;
-        valid_scan_code_temp_next = valid_scan_code_temp;
+        valid_scan_code_temp = 1'b0;
 
-        if (counter == 4'd11) begin
+        if (counter == 4'd11) begin : counter_logic
             counter_next = '0;
-            valid_scan_code_temp_next = 1'b1;
+            valid_scan_code_temp= 1'b1;
         end else begin
-            valid_scan_code_temp_next = '0;
+            valid_scan_code_temp = 1'b0;
         end
 
-        if (edge_found) begin // do the shifting here
+        if (edge_found) begin : shift_logic // do the shifting here
             scan_code_temp_next = {serial_data, scan_code_temp[9:1]}; // right shift
             counter_next = counter + 1;
         end
