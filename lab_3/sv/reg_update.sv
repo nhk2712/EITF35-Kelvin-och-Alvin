17dd83d1dc5d48e03421b2908c286e0c0cc35504
Khanh Nguyen <nguyenhoangkhanh271205@gmail.com>
2025-09-29 15:22:51 +0200

    Done reg_update

--- lab_3/sv/reg_update.sv
+++ lab_3/sv/reg_update.sv
@@ -9,4 +9,29 @@ module reg_update (
     output logic [7:0] b
     );
 
+    logic [7:0] reg_a, reg_a_next, reg_b, reg_b_next;
+
+    always_ff @(posedge clk or posedge rst) begin : Sequential
+        if (rst) begin
+            reg_a <= '0;
+            reg_b <= '0;
+        end else begin
+            reg_a <= reg_a_next;
+            reg_b <= reg_b_next;
+        end
+    end
+
+    always_comb begin : Combinational
+        // Default
+        reg_a_next = reg_a;
+        reg_b_next = reg_b;
+
+        // Conditional
+        if (reg_ctrl[0]) reg_a_next = reg_input;
+        if (reg_ctrl[1]) reg_b_next = reg_input;
+    end
+
+    assign a = reg_a;
+    assign b = reg_b;
+
 endmodule
