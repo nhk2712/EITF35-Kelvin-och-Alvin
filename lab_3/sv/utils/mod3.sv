c4bc46e4da73fccf5a521a7204611e68e31b6b78
Khanh Nguyen <nguyenhoangkhanh271205@gmail.com>
2025-10-01 15:44:09 +0200

    Correct mod3 module

--- lab_3/sv/utils/mod3.sv
+++ lab_3/sv/utils/mod3.sv
@@ -7,13 +7,13 @@ module mod3(
 );
     logic [7:0] x1, x2, x3, x4, x5, x6, x7; // Internal signals after each cell, there are 7 cascading cells
     
-    mod3_cell cell_1(.inp_A(inp_A), .theta(8'd192), .out_x(x1));
-    mod3_cell cell_2(.inp_A(x1), .theta(8'd96), .out_x(x2));
-    mod3_cell cell_3(.inp_A(x2), .theta(8'd48), .out_x(x3));
-    mod3_cell cell_4(.inp_A(x3), .theta(8'd24), .out_x(x4));
-    mod3_cell cell_5(.inp_A(x4), .theta(8'd12), .out_x(x5));
-    mod3_cell cell_6(.inp_A(x5), .theta(8'd6), .out_x(x6));
-    mod3_cell cell_7(.inp_A(x6), .theta(8'd3), .out_x(x7));
+    mod3_cell cell_1(.inp_A(inp_A), .inp_theta(8'd192), .out_x(x1));
+    mod3_cell cell_2(.inp_A(x1), .inp_theta(8'd96), .out_x(x2));
+    mod3_cell cell_3(.inp_A(x2), .inp_theta(8'd48), .out_x(x3));
+    mod3_cell cell_4(.inp_A(x3), .inp_theta(8'd24), .out_x(x4));
+    mod3_cell cell_5(.inp_A(x4), .inp_theta(8'd12), .out_x(x5));
+    mod3_cell cell_6(.inp_A(x5), .inp_theta(8'd6), .out_x(x6));
+    mod3_cell cell_7(.inp_A(x6), .inp_theta(8'd3), .out_x(x7));
 
     assign out_r = x7[1:0];
 endmodule
